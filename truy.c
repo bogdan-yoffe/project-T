9278457c9ff1786499d6cdc57986db8991cc2093
bogdan-yoffe <bogdanioffe07@gmail.com>
2025-09-17 22:08:31 +0300

    Update truy.c

---
+++ truy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    int name[3];    
+    int values[3];  
+    float avereg;
+    int mone=0;
+   
+    for (i = 0; i < 3; i++) {
+        name[i] = i + 1; 
+        printf("(%d) enter number: ", name[i]);
+        scanf("%d", &values[i]); 
+        mone=mone+values[i];
+    }
+    avereg = (float)mone/3;
+    printf("the avereg is :%.2f",avereg);
+
+    return 0; 
+}
